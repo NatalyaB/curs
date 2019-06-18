8466fde0f9b9f0f6ba7388a589c526e4bd6318a1
Natalya <nata.basovska@gmail.com>
2019-06-18 14:11:43 -0400

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('#mycarousel'). carousel({ interval: 2000 });
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        };
+    });
+            // Reserve Modal
+    $('#reserveButton').click(function() {
+        $('#reserveModal').modal('show');
+    });
+
+    $('.close-reserve-modal').click(function() {
+        $('#reserveModal').modal('hide');
+    });
+
+            // Login Modal
+    $('#loginButton').click(function() {
+        $('#loginModal').modal('show');
+    });
+
+    $('.close-login-modal').click(function() {
+        $('#loginModal').modal('hide');
+    });
+});
